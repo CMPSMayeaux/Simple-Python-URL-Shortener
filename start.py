1cb664c0d3d661155ab0eccca96a6846dc7f0da3
Klaus Mayeaux <me@thenewport.dev>
2022-04-23 22:35:29 -0500

    Add files via upload

---
+++ start.py
@@ -0,0 +1,16 @@
+# Program Name: "URL Shortener GUI"
+# Date(s) Developed: 23 April 2022 to PRESENT
+# Developer: Klaus Mayeaux
+# Developer Email: me@thenewport.dev
+# Developer Website: https://TheNewport.dev
+# Developer Twitter: @CMPSMayeaux
+# Developer GitHub: @CMPSMayeaux
+
+# Import(s):
+import tkinter as tk
+from GUI import mainWindow
+
+# Execute the GUI
+mainWindow
+
+
